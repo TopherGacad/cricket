a809fb5428fc02d3373635cd520dfd41b4f98e36
TopherGacad <gacadtophs@gmail.com>
2024-09-06 17:40:16 +0800

    created the partial UI for the login

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,8 +1,19 @@
 export default function login() {
     return (
-      <div className="h-screen w-screen bg-cover bg-login-bg flex justify-center items-center m-0">
-        <div className="bg-white h-[500px] w-[500px] border-solid border-[1px] border-[#D9D9D9] rounded-[10px] flex justify-center">
-          hello
+      <div className="h-screen w-screen bg-cover bg-no-repeat bg-login-bg flex justify-center items-center m-0">
+        <div className="bg-white h-[600px] w-[550px] border-solid border-[1px] border-[#D9D9D9] rounded-[10px] flex justify-center">
+          <div className="pt-[70px] h-[120px] w-full flex flex-col justify-center items-center">
+            <h1 className="text-4xl"><b><span className="text-[#61DADA]">Welcome</span> to Cricket</b></h1>
+            <div className="text-center text-[15px] pt-2 leading-tight">
+              <p>Catch every issue, everytime. </p>
+              <p>Login to your account or contact the admin for</p>
+              <p>account creation.</p>
+            </div>
+          </div>
+
+          <div className="">
+            {/**FORM HERE **/}
+          </div>
         </div>
       </div>
     );
