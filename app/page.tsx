514db0c38b1f4d6f7b46f17e5c8628c2cf8d9a20
TopherGacad <gacadtophs@gmail.com>
2024-09-17 09:32:37 +0800

    fix root page config

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,18 @@ import Cookies from 'js-cookie';  // For cookie management
 const Home = () =>{
   const router = useRouter();
 
-  // useEffect(() =>{
-  //   router.push('/login'); //redirect to login page when the localhost:3000 or home is access
-  // }, [router]);
-
-  useEffect(() => {
-    const authToken = Cookies.get('authToken');
-    if (authToken) {
-      router.push('/dashboard'); // Redirect to dashboard if already logged in
-    }else {
-      router.push('/login');
-    }
+  useEffect(() =>{
+    router.push('/login'); //redirect to login page when the localhost:3000 or home is access
   }, [router]);
+
+  // useEffect(() => {
+  //   const authToken = Cookies.get('authToken');
+  //   if (authToken) {
+  //     router.push('/dashboard'); // Redirect to dashboard if already logged in
+  //   }else {
+  //     router.push('/login');
+  //   }
+  // }, [router]);
 
   return null; //render nothing from this page
 }
