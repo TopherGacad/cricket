8ddc82be298b90cd58041fe4d8980c1e0acf65bc
TopherGacad <gacadtophs@gmail.com>
2024-10-16 17:17:01 +0800

    Test rbac conditional

--- app/sample.tsx
+++ app/sample.tsx
@@ -1,20 +1,20 @@
-"use client";
+// "use client";
 
-import { useRouter, usePathname } from 'next/navigation';
-import { useEffect } from 'react';
+// import { useRouter, usePathname } from 'next/navigation';
+// import { useEffect } from 'react';
 
-function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const pathname = usePathname();
+// function MyApp({ Component, pageProps }) {
+//   const router = useRouter();
+//   const pathname = usePathname();
 
-  useEffect(() => {
-    // Redirect logic (ensure it doesn't wrongly trigger for refreshes)
-    if (!pathname.startsWith("/dashboard")) {
-      router.push("/dashboard");
-    }
-  }, [pathname, router]);
+//   useEffect(() => {
+//     // Redirect logic (ensure it doesn't wrongly trigger for refreshes)
+//     if (!pathname.startsWith("/dashboard")) {
+//       router.push("/dashboard");
+//     }
+//   }, [pathname, router]);
 
-  return <Component {...pageProps} />;
-}
+//   return <Component {...pageProps} />;
+// }
 
-export default MyApp;
+// export default MyApp;
